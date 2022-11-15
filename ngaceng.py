a64aa423c0d2c3bb5f8d3e2145feb3579e1b1195
Irfan-XD <114362956+irfan-XD@users.noreply.github.com>
2022-11-16 01:33:49 +0700

    Add files via upload

---
+++ ngaceng.py
@@ -0,0 +1,25 @@
+#coding by Irfan azhura XD
+#buat hek bulan bang
+import os,sys
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+h = '\x1b[1;92m' # HIJAU +
+def azhura():
+  print(f'''{m}
+_________                       __    
+\_   ___ \____________    ____ |  | __
+/    \  \/\_  __ \__  \ _/ ___\|  |/ /
+\     \____|  | \// __ \\  \___|  <{x} 
+ \______  /|__|  (____  /\___  >__|_ \
+        \/            \/     \/     \/
+AUTHOR : {h}irfan azhura xD{x}
+STATUS : {h}ngaceng{x}
+GITHUB : {h}github.com/irfan-XD{x}
+ ''')
+azhura()
+print(' =============================')
+print(f' [{h}1{x}].star crack')
+irfanXD = input(f' [{h}•{x}] pilih : ')
+if irfanXD in ['1']:
+     os.system('xdg-open https://bokepind.net/tag/bokep-indo/')
